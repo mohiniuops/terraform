ff746aeed306b8482212fd365970a434f87a3dc4
mohiniu <mohini4job@gmail.com>
2020-08-03 15:38:27 +0530

    Implicit dependencies Demo using elastic ip bucket & ec2 instance

---
+++ implicit-dependency.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+resource "aws_instance" "terraform_ec2" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.terraform_ec2.id
+}
